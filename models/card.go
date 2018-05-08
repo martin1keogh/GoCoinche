bdedd06236b3ddaa13bc3f81509bd48e1e3f0176
Martin Keogh <martin1keogh@gmail.com>
2018-05-08 23:45:00 +0200

    Add Card & Suits models

---
+++ models/card.go
@@ -0,0 +1,36 @@
+package models
+
+type Suit string
+
+const (
+	Hearts   Suit = "Hearts"
+	Diamonds Suit = "Diamonds"
+	Clubs    Suit = "Clubs"
+	Spades   Suit = "Spades"
+)
+
+func AllSuits() [4]Suit {
+	return [...]Suit{Hearts, Diamonds, Clubs, Spades}
+}
+
+type CardName string
+
+const (
+	_7  CardName = "Seven"
+	_8  CardName = "Eight"
+	_9  CardName = "Nine"
+	_10 CardName = "Ten"
+	_J  CardName = "Jack"
+	_Q  CardName = "Queen"
+	_K  CardName = "King"
+	_As CardName = "As"
+)
+
+func AllCardNames() [8]CardName {
+	return [8]CardName{_7, _8, _9, _10, _J, _Q, _K, _As}
+}
+
+type Card struct {
+	suit Suit
+	name CardName
+}
