4c39676149a8f56e8a5a7957b5d784ca14a746af
Martin Keogh <martin1keogh@gmail.com>
2018-05-10 10:48:36 +0200

    Add helper method table.ForeachPositionStartingFrom

--- internal/models/table.go
+++ internal/models/table.go
@@ -47,30 +47,34 @@ func (t *Table) Deal() {
 	newHands := t.deck.deal()
 	i := 0
 
-	// Pass the dealer chip to the next player
-	// TODO see if it's not better to call this at the end of
-	// a board instead of here
-	t.dealer = *t.dealer.Next()
-
 	// We first deal cards to the player after the current dealer,
 	// so we have to call Next() one more time.
-	t.dealer.Next().Do(func(d interface{}) {
-		direction := d.(Direction)
-		position := t.findPosition(direction)
-		position.Hand = newHands[i]
+	firstReceiver := t.dealer.Next().Value.(Direction)
+
+	t.ForeachPositionStartingFrom(firstReceiver, func(pos *Position) {
+		pos.Hand = newHands[i]
 		i += 1
 	})
 }
 
-func (t *Table) findPosition(d Direction) Position {
+func (t *Table) findPosition(d Direction) *Position {
 	switch d {
 	case North:
-		return t.NorthSouth.position1
+		return &t.NorthSouth.position1
 	case South:
-		return t.NorthSouth.position2
+		return &t.NorthSouth.position2
 	case East:
-		return t.EastWest.position1
+		return &t.EastWest.position1
 	default: // O real enums how I miss thee
-		return t.EastWest.position2
+		return &t.EastWest.position2
 	}
+}
+
+func (t *Table) ForeachPositionStartingFrom(start Direction, action func(*Position)) {
+	ring := GetPlayerOrder(start)
+	ring.Next().Do(func(i interface{}) {
+		dir := i.(Direction)
+		pos := t.findPosition(dir)
+		action(pos)
+	})
 }
